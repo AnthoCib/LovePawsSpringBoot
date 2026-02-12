f1e821bdff415831fa4988c71df413282a464305
AnthoCib <anthoncr74@gmail.com>
2026-02-12 00:42:46 -0500

    .

---
+++ src/main/java/com/lovepaws/app/adopcion/repository/AdopcionRepository.java
@@ -0,0 +1,19 @@
+package com.lovepaws.app.adopcion.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lovepaws.app.adopcion.domain.Adopcion;
+
+public interface AdopcionRepository  extends JpaRepository<Adopcion, Integer>{
+	
+	List<Adopcion> findByUsuarioAdoptante_Id(Integer idUsuarioAdoptante);
+
+	List<Adopcion> findByMascotaId(Integer mascotaId);
+
+	boolean existsBySolicitud_Id(Integer solicitudId);
+
+	long countByEstado_Id(String estadoId);
+
+}
