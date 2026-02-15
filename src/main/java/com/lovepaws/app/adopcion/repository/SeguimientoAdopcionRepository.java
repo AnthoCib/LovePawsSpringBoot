07ec2daf74bdff0d99a440c1b6924d60a30b1ab8
AnthoCib <anthoncr74@gmail.com>
2026-02-15 18:52:28 -0500

    Merge branch 'codex/revise-adopcion-y-seguimiento-post-adopcion' of https://github.com/AnthoCib/LovePawsSpringBoot.git into codex/revise-adopcion-y-seguimiento-post-adopcion

--- src/main/java/com/lovepaws/app/adopcion/repository/SeguimientoAdopcionRepository.java
+++ src/main/java/com/lovepaws/app/adopcion/repository/SeguimientoAdopcionRepository.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.lovepaws.app.adopcion.domain.SeguimientoAdopcion;
 
+
+@Repository("seguimientoPostAdopcionRepositoryAdopcion")
 public interface SeguimientoAdopcionRepository extends JpaRepository<SeguimientoAdopcion, Integer> {
 
     @Query("""
