289b8a66f4ab5a5e3fbb0e52c5c2c207ed66f180
AnthoCib <anthoncr74@gmail.com>
2026-02-21 15:44:13 -0500

    ..

--- src/main/java/com/lovepaws/app/config/CloudinaryConfig.java
+++ src/main/java/com/lovepaws/app/config/CloudinaryConfig.java
@@ -11,15 +11,22 @@ import com.cloudinary.utils.ObjectUtils;
 @Configuration
 public class CloudinaryConfig {
 	
-	   @Bean
-	    public Cloudinary cloudinary(
-	            @Value("${cloudinary.cloud-name}") String cloudName,
-	            @Value("${cloudinary.api-key}") String apiKey,
-	            @Value("${cloudinary.api-secret}") String apiSecret) {
 
-	        return new Cloudinary(ObjectUtils.asMap(
-	                "cloud_name", cloudName,
-	                "api_key", apiKey,
-	                "api_secret", apiSecret));
-	    }
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", cloudName,
+            "api_key", apiKey,
+            "api_secret", apiSecret
+        ));
+    }
 }
